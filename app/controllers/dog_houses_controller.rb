d09cb58b380ed6b67b3e760b17536eda4d5b2ad5
Rodrigo Alba <rodrigo@megabadvisors.com>
2021-12-19 17:17:06 -0400

    commit tree d3d417fe9a17c6b556318bffd639b8c2fc44ff90

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -2,7 +2,7 @@ class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
-    dog_house = DogHouse.find(params[:id])
+    dog_house = find_dog_house
     render json: dog_house
   end
 
@@ -11,5 +11,15 @@ class DogHousesController < ApplicationController
   def render_not_found_response
     render json: { error: "Dog house not found" }, status: :not_found
   end
+
+  def find_dog_house
+    DogHouse.find(params[:id])
+  end
+
+  def find_review
+    Review.find(params[:id])
+  end
+  
+  
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,12 +2,17 @@ class ReviewsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    reviews = Review.all
+    if params[:dog_house_id]
+      dog_house = find_dog_house
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
     render json: reviews, include: :dog_house
   end
 
   def show
-    review = Review.find(params[:id])
+    review = find_review
     render json: review, include: :dog_house
   end
 
@@ -25,5 +30,15 @@ class ReviewsController < ApplicationController
   def review_params
     params.permit(:username, :comment, :rating)
   end
+
+  def find_review
+    Review.find(params[:id])
+  end
+
+  def find_dog_house
+    DogHouse.find(params[:dog_house_id])
+  end
+  
+  
 
 end
